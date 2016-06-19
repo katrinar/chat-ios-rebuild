fcdc750e2975228cab8a6c7e8707aaeb0b9ac2be
Brian <briancorrea@gmail.com>
2016-06-19 16:24:17 -0400

    Adding Constants File

---
+++ chat-ios/Config.swift
@@ -0,0 +1,14 @@
+//
+//  Config.swift
+//  chat-ios
+//
+//  Created by Brian Correa on 6/19/16.
+//  Copyright © 2016 Velocity360. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let baseUrl = "http:localhost:3000"
+}

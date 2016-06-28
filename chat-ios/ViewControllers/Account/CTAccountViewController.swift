b026933fb0e2d421ce314f37cfdf2ed53234767f
Brian <briancorrea@gmail.com>
2016-06-27 22:56:35 -0400

    Connecting Buttons to Appropriate VCs

--- chat-ios/ViewControllers/Account/CTAccountViewController.swift
+++ chat-ios/ViewControllers/Account/CTAccountViewController.swift
@@ -25,59 +25,85 @@ class CTAccountViewController: CTViewController {
     override func loadView() {
         let frame = UIScreen.mainScreen().bounds
         let view = UIView(frame: frame)
-        view.backgroundColor = .blueColor()
+        view.backgroundColor = UIColor.redColor()
         
-        let padding = CGFloat(20)
-        let width = frame.size.width-2*padding
-        let height = CGFloat(44)
-        let bgColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.65)
-        let whiteColor = UIColor.whiteColor().CGColor
-        let font = UIFont(name: "Heiti SC", size: 18)
-        var y = CGFloat(120)
-        
-        let buttonTitles = ["Sign Up", "Login"]
-        for btnTitle in buttonTitles {
-            let btn = UIButton(frame: CGRect(x: padding, y: y, width: width, height: height))
-            btn.setTitle(btnTitle, forState: .Normal)
-            btn.backgroundColor = bgColor
-            btn.layer.borderColor = whiteColor
-            btn.layer.borderWidth = 2
-            btn.layer.cornerRadius = 22
-            btn.titleLabel?.font = font
-            
-            btn.addTarget(self, action: #selector(CTAccountViewController.buttonTapped(_:)), forControlEvents: .TouchUpInside)
-            self.loginButtons.append(btn)
-            view.addSubview(btn)
-            
-            y += height+padding
+        if (CTViewController.currentUser.id == nil){ // not logged in
+            self.loadSignUpView(frame, view: view)
         }
+        else{ //logged in
+            self.loadAccountView(frame, view: view)
+        }
+        
         self.view = view
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        self.navigationItem.hidesBackButton = true
     }
     
+    func loadAccountView(frame: CGRect, view: UIView){
+        
+        let padding = CGFloat(Constants.padding)
+        let width = frame.size.width-2*padding
+        let y = CGFloat(Constants.origin_y)
+        
+        let nameLabel = UILabel(frame: CGRect(x: padding, y: y, width: width, height: 22))
+        nameLabel.text = CTViewController.currentUser.email //change to username
+        view.addSubview(nameLabel)
+        
+    }
+    
+    func loadSignUpView(frame: CGRect, view: UIView){
+        
+        let padding = CGFloat(Constants.padding)
+        let width = frame.size.width-2*padding
+        let height = CGFloat(44)
+        var y = CGFloat(Constants.origin_y)
+        
+        let buttonTitles = ["Sign Up", "Login"]
+        for btnTitle in buttonTitles {
+            let btn = CTButton(frame: CGRect(x: padding, y: y, width: width, height: height))
+            btn.setTitle(btnTitle, forState: .Normal)
+            btn.addTarget(self, action: #selector(CTAccountViewController.buttonTapped(_:)), forControlEvents: .TouchUpInside)
+            view.addSubview(btn)
+            self.loginButtons.append(btn)
+            y += height + padding
+        }
+        
+    }
     
     func buttonTapped(btn: UIButton){
         let buttonTitle = btn.titleLabel?.text?.lowercaseString
         print("buttonTapped: \(buttonTitle!)")
         
-        if (buttonTitle == "sign up"){
+        if(buttonTitle == "sign up"){
             let registerVc = CTRegisterViewController()
             self.navigationController?.pushViewController(registerVc, animated: true)
         }
         
-        if (buttonTitle == "login"){
+        if(buttonTitle == "login"){
+            let loginVc = CTLoginViewController()
+            self.navigationController?.pushViewController(loginVc, animated: true)
+        }
+    }
+    
+    override func userLoggedIn(notification: NSNotification){
+        super.userLoggedIn(notification)
+        
+        if(CTViewController.currentUser.id == nil){
             return
         }
+        
+        for btn in self.loginButtons{
+            btn.alpha = 0
+        }
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 }

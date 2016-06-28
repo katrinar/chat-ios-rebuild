69045663c81f46bc0af41bf2400f53376234e35c
Brian <briancorrea@gmail.com>
2016-06-27 22:54:26 -0400

    Pushing Vc to Register/Login Buttons

--- chat-ios/CTViewController.swift
+++ chat-ios/CTViewController.swift
@@ -10,8 +10,42 @@ import UIKit
 
 class CTViewController: UIViewController {
     
+    static var currentUser = CTProfile() //shared across application
+    
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?){
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.addObserver(self,
+                                       selector: #selector(CTViewController.userLoggedIn(_:)),
+                                       name: Constants.kUserLoggedInNotification,
+                                       object: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
+    
+    func userLoggedIn(notification: NSNotification){
+        if let user = notification.userInfo!["user"] as? Dictionary<String, AnyObject>{
+            CTViewController.currentUser.populate(user)
+        }
+    }
+    
+    func postLoggedInNotification(currentUser: Dictionary<String, AnyObject>){
+        let notification = NSNotification(
+            name: Constants.kUserLoggedInNotification,
+            object: nil,
+            userInfo: ["user":currentUser]
+        )
+        
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.postNotification(notification)
         
     }
     

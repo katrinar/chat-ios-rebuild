cfc26c9bb9fc62e654feacf133b7c68668a3d719
Brian <briancorrea@gmail.com>
2016-07-12 16:12:04 -0400

    Parsing formatted date

--- chat-ios/models/CTPost.swift
+++ chat-ios/models/CTPost.swift
@@ -15,6 +15,7 @@ class CTPost: NSObject {
     var place: String!
     var from: String!
     var timestamp: NSDate!
+    var formattedDate: String!
     
     func populate(postInfo: Dictionary<String, AnyObject!>){
         let keys = ["message", "place", "from"]
@@ -23,11 +24,14 @@ class CTPost: NSObject {
         }
         
         if let _timestamp = postInfo["timestamp"] as? String {
-            //print("TIMESTAMP: \(_timestamp)")
             
             let ts = NSTimeInterval(_timestamp)
             self.timestamp = NSDate(timeIntervalSince1970: ts!)
             print("DATE: \(self.timestamp)")
+            
+            let dateFormatter = NSDateFormatter()
+            dateFormatter.dateFormat = "MMM dd, yyyy" // "May 16, 2015"
+            self.formattedDate = dateFormatter.stringFromDate(self.timestamp)
         }
     }
     

5aa5d271d8a2ea8f234a31023f22f87150b060c5
Brian <briancorrea@gmail.com>
2016-07-19 15:32:38 -0400

    Adding Image keys and parsing Cloudinary

--- chat-ios/models/CTPost.swift
+++ chat-ios/models/CTPost.swift
@@ -9,13 +9,21 @@
 import UIKit
 
 class CTPost: NSObject {
-    
+
     var id: String!
     var message: String!
-    var place: String!
-    var from: String!
+    var from: Dictionary<String, AnyObject>!
     var timestamp: NSDate!
     var formattedDate: String!
+    var place: Dictionary<String,AnyObject>!
+    
+    //Images:
+    var image: Dictionary<String,AnyObject>!
+    var thumbnailUrl: String!
+    var thumbnailData: UIImage?
+    var imageUrl: String!
+    var imageData: UIImage?
+    var isFetching = false
     
     func populate(postInfo: Dictionary<String, AnyObject!>){
         let keys = ["message", "place", "from"]
@@ -23,16 +31,25 @@ class CTPost: NSObject {
             self.setValue(postInfo[key], forKey: key)
         }
         
+        //Image Strings:
+        if let _image = postInfo["image"] as? Dictionary<String, AnyObject>{
+            if let _original = _image["original"] as? String {
+                self.imageUrl = _original
+            }
+            
+            if let _thumb = _image["thumb"] as? String {
+                self.thumbnailUrl = _thumb
+            }
+        } 
+        
         if let _timestamp = postInfo["timestamp"] as? String {
             
             let ts = NSTimeInterval(_timestamp)
             self.timestamp = NSDate(timeIntervalSince1970: ts!)
-            print("DATE: \(self.timestamp)")
             
             let dateFormatter = NSDateFormatter()
             dateFormatter.dateFormat = "MMM dd, yyyy" // "May 16, 2015"
             self.formattedDate = dateFormatter.stringFromDate(self.timestamp)
         }
     }
-    
 }
